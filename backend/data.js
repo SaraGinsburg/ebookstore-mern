606933a60b605d193cfa20611093459b93cd695b
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2022-04-06 21:15:58 -0400

    created sample users

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,21 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Sara',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Joe',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+
   products: [
     {
       // _id: '1',
@@ -72,4 +89,5 @@ const data = {
     },
   ],
 };
+
 export default data;
